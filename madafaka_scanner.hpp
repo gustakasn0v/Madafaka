e119590a9b50d8b661f68dd38de20e4525ed19ff
Gustavo El Khoury <gustavo@hyrule.(none)>
2014-02-19 18:31:48 -0430

    Commit inicial

---
+++ madafaka_scanner.hpp
@@ -0,0 +1,34 @@
+#ifndef __MADAFAKASCANNER_HPP__
+#define __MADAFAKASCANNER_HPP__ 1
+ 
+#if ! defined(yyFlexLexerOnce)
+#include <FlexLexer.h>
+#endif
+ 
+#undef YY_DECL
+#define YY_DECL int MC::MC_Scanner::yylex()
+ 
+//#include "mc_parser.tab.hh"
+ 
+namespace Madafaka{
+ 
+class Madafaka_Scanner : public yyFlexLexer{
+	public:
+		MC_Scanner(std::istream *in) : 
+			yyFlexLexer(in),
+			ylval( nullptr ){};
+		int yylex(MC::MC_Parser::semantic_type *lval){
+			yylval = lval;
+			return( yylex() );
+		}
+
+	private:
+		/* hide this one from public view */
+		int yylex();
+		/* yyval ptr */
+		Madafaka::Madafaka_Parser::semantic_type *yylval;
+		};
+		 
+} /* end namespace Madafaka */	
+#endif /* END __MCSCANNER_HPP__ */
+ 
